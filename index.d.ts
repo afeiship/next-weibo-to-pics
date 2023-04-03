8cc4cc63f173c9f74dfc42e79945fadc15276241
afeiship <1290657123@qq.com>
2023-04-03 10:13:29 +0800

    feat: typings

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,18 @@
+interface ResponseItem {
+  width: number;
+  size: number;
+  ret: number;
+  height: number;
+  name: string;
+  pid: string;
+  format: string;
+  url: string;
+}
+
+interface Options {
+  baseURL?: string;
+}
+
 interface NxStatic {
-  // your codes goes here.
+  weiboToPics(html: string, opts?: Options): ResponseItem[];
 }
